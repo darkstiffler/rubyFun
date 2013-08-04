1fc5a5e868d4943a1586a9fd3ed8681b61bb63ca
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-03 22:47:09 -0700

    add more commentary

--- app/assets/javascripts/color_changer.js
+++ app/assets/javascripts/color_changer.js
@@ -1,36 +1,43 @@
 $(document).ready(function(){
 	console.log('Color_Changer.js :Loaded:');
+
+	// the following changes the colors upon clicking the related button
 	
+	// turns the box red
 	$('.red').click(function(){
 	$('#container').css("background-color", "red");
 	$('#container').css("color", "black");
 	});
 
+	// turns the box blue
 	$('.blue').click(function(){
 	$('#container').css("background-color", "blue");
 	$('#container').css("color", "black");
 	});
 
+	// turns the box green
 	$('.green').click(function(){
 	$('#container').css("background-color", "green");
 	$('#container').css("color", "black");
 	});
-
+	// turns the box purple
 	$('.purple').click(function(){
 	$('#container').css("background-color", "purple");
 	$('#container').css("color", "black");
 	});
-
+	// turns the box yellow
 	$('.yellow').click(function(){
 	$('#container').css("background-color", "yellow");
 	$('#container').css("color", "black");
 	});
-	
+
+	// turns the box black	
 	$('.black').click(function(){
 	$('#container').css("background-color", "black");
 	$('#container').css("color", "white");
 	});
 
+	// turns the box pink
 	$('.pink').click(function(){
 	$('#container').css("background-color", "pink");
 	$('#container').css("color", "black");
